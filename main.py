d9afd4207f82ca8a5fb6fd6cf45444d607321eaa
Abe <abe@habet.dev>
2022-10-02 03:42:49 -0400

    Change to bot command and added event from meetup

--- main.py
+++ main.py
@@ -1,14 +1,70 @@
+# This example requires the 'message_content' privileged intent to function.
 import os
 
 import discord
+from discord.ext import commands
+
+from meetup_rest_api import fetch_meetup_events_detail
 
 DISCORD_TOKEN = os.getenv("BARISTA_TOKEN")
 
+# Defines a custom Select containing colour options
+# that the user can choose. The callback function
+# of this class is called when the user changes their choice
+class Dropdown(discord.ui.Select):
+    def __init__(self, scheduled_events: dict):
 
-class MyClient(discord.Client):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        # Set the options that will be presented inside the dropdown
+        self.scheduled_events = scheduled_events
+        options = []
+        for event_id, event in scheduled_events.items():
+            options.append(
+                discord.SelectOption(
+                    value=event_id,
+                    label=event["name"],
+                    description=event["start_time"].strftime("%m/%d/%Y"),
+                )
+            )
 
+        # The placeholder is what will be shown when no option is chosen
+        # The min and max values indicate we can only pick one of the three options
+        # The options parameter defines the dropdown options. We defined this above
+        super().__init__(
+            placeholder="Choose your event",
+            min_values=1,
+            max_values=len(scheduled_events),
+            options=options,
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        # Use the interaction object to send a response message containing
+        # the user's favourite colour or choice. The self object refers to the
+        # Select object, and the values attribute gets a list of the user's
+        # selected options. We only want the first one.
+        guild = interaction.guild
+        for event_id in self.values:
+            event = self.scheduled_events[event_id]
+            await guild.create_scheduled_event(**event)
+
+        await interaction.response.send_message(f"{len(self.values)} event(s) created")
+
+
+class DropdownView(discord.ui.View):
+    def __init__(self, scheduled_events: dict):
+        super().__init__()
+
+        # Adds the dropdown to our view object.
+        self.add_item(Dropdown(scheduled_events))
+
+
+class Bot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.message_content = True
+
+        super().__init__(
+            command_prefix=commands.when_mentioned_or("!"), intents=intents
+        )
         self.role_message_id = 960556131766398986  # ID of the message that can be reacted to to add/remove a role.
         self.emoji_to_role = {
             discord.PartialEmoji(
@@ -104,9 +160,31 @@ class MyClient(discord.Client):
             # If we want to do something in case of errors we'd do it here.
             pass
 
+    async def on_ready(self):
+        print(f"Logged in as {self.user} (ID: {self.user.id})")
+        print("------")
 
-intents = discord.Intents.default()
-intents.members = True
 
-client = MyClient(intents=intents)
-client.run(DISCORD_TOKEN)
+bot = Bot()
+
+
+@bot.command()
+@commands.has_any_role("Admin", "Organizer")
+async def event(ctx, chapter=None):
+    """Sends a message with our dropdown containing colours"""
+
+    if chapter is not None:
+        scheduled_events = fetch_meetup_events_detail(chapter)
+
+        # Create the view containing our dropdown
+        view = DropdownView(scheduled_events)
+
+        # Sending a message containing our view
+        await ctx.send("Pick your event(s):", view=view)
+    else:
+        await ctx.send(
+            "Pull meetup.com events over to discord\n\nUsage: !event <INSERT MEETUP.COM GROUP URL>"
+        )
+
+
+bot.run(DISCORD_TOKEN)
