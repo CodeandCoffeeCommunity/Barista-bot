33997bdf8d211b1940b78c9840aabc4206f4ffdc
Abe <abe@habet.dev>
2022-12-04 14:39:37 -0500

    add button roles
    the design was inspired by the example over at https://github.com/Pycord-Development/pycord/blob/master/examples/views/button_roles.py

--- main.py
+++ main.py
@@ -2,12 +2,97 @@
 import os
 
 import discord
+from string import Template
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv("BARISTA_TOKEN")
+
+message_content = Template("""
+Welcome to **Code & Coffee**!
+We're a community of developers looking to grow and make new friends.
+
+**What is Code & Coffee?**
+We are the #1 in-person community on the Meetup.com platform. Our community hosts events typically twice a month, where we have self-taught hackers, current/ex FANG engineers, uni students, startup devs, fintech, bootcamp grads, and non-traditional career-switches of all ages, and more! If you do anything with an IDE you belong. Expect to meet new people, get job referrals, and see people hacking on cool side projects! We expect you to oblige by the Code of Conduct conveniently in ${rules}.
+
+**1. New member - Onboarding >> START HERE<<**
+To view the rest of the server, click your city's colored button below:
+
+👈 You'll see your city's Discord channel on the left sidebar. 
+
+**2. Introduce yourself in ${intro_circle} channel** 
+
+**3. Say "Hi" to your local developers in the left sidebar! (optional)**
+
+Don't have a local chapter? Message @Steve Chen we have a "just add human ✨ " C&C starter kit ready for you, a support network of chapter leaders, and resources that'll guide you through it. 
+""")
+
+# This is the list of role IDs that will be added as buttons.
+role_ids = [
+    (1020907447822581823, discord.ButtonStyle.primary),  # Chicago
+    (1020906856136314901, discord.ButtonStyle.gray),  # NYC
+    (1020907461412143135, discord.ButtonStyle.danger),  # Austin
+    (1020907860185579530, discord.ButtonStyle.green),  # Columbus
+    (1020908145712828518, discord.ButtonStyle.blurple),  # St. Louis
+    (1021599522964639796, discord.ButtonStyle.red),  # Whidbey island
+    (1021495205641338950, discord.ButtonStyle.success),  # Virtual / Online
+    (1024892173310767186, discord.ButtonStyle.gray),  # DC/MD/VA
+    (1027739489302495314, discord.ButtonStyle.success),  # Cincinatti
+    (1032165548009721917, discord.ButtonStyle.green),  # Seattle
+    (1033893140270153749, discord.ButtonStyle.primary),  # Boston
+    (1025226011832483880, discord.ButtonStyle.secondary),  # San Francisco
+    (1036290493698555985, discord.ButtonStyle.danger),  # Atlanta
+    (1039006815599480873, discord.ButtonStyle.success),  # Ventura
+    (1042302822508658698, discord.ButtonStyle.danger),  # Triangle
+    (1040183449627152396, discord.ButtonStyle.success),  # Milwaukee
+]
+
+
+class RoleButton(discord.ui.Button):
+    def __init__(self, role: discord.Role, style=discord.ButtonStyle.primary):
+        """A button for one role. `custom_id` is needed for persistent views."""
+        super().__init__(
+            label=role.name,
+            style=style,
+            custom_id=str(role.id),
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        """
+        This function will be called any time a user clicks on this button.
+        Parameters
+        ----------
+        interaction: :class:`discord.Interaction`
+            The interaction object that was created when a user clicks on a button.
+        """
+        # Get the user who clicked the button.
+        user = interaction.user
+        # Get the role this button is for (stored in the custom ID).
+        role = interaction.guild.get_role(int(self.custom_id))
+
+        if role is None:
+            # If the specified role does not exist, return nothing.
+            # Error handling could be done here.
+            return
+
+        # Add the role and send a response to the user ephemerally (hidden to other users).
+        if role not in user.roles:
+            # Give the user the role if they don't already have it.
+            await user.add_roles(role)
+            await interaction.response.send_message(
+                f"🎉 You have been given the role {role.mention}!",
+                ephemeral=True,
+            )
+        else:
+            # Otherwise, take the role away from the user.
+            await user.remove_roles(role)
+            await interaction.response.send_message(
+                f"❌ The {role.mention} role has been taken from you!",
+                ephemeral=True,
+            )
+
 
 class Bot(commands.Bot):
     def __init__(self):
@@ -148,7 +233,27 @@ class Bot(commands.Bot):
     async def on_ready(self):
         print(f"Logged in as {self.user} (ID: {self.user.id})")
         print("------")
+        # We recreate the view as we did in the /post command.
+        view = discord.ui.View(timeout=None)
+        # Make sure to set the guild ID here to whatever server you want the buttons in!
+        guild = self.get_guild(894703368411422790)
+        for role_id, style in role_ids:
+            role = guild.get_role(role_id)
+            view.add_item(RoleButton(role, style))
 
+        # Add the view to the bot so that it will watch for button interactions.
+        self.add_view(view)
+        channel = self.get_channel(960555939579195473)
+        link_str_data = {
+                "rules": self.get_channel(960540110477222008).mention,
+                "intro_circle": self.get_channel(1020074229804302468).mention,
+                }
+        # message_content = "Click a button to assign yourself a city role"
+        try:
+            message = await channel.fetch_message(1049035230880743515)
+            await message.edit(content=message_content.substitute(link_str_data), view=view)
+        except discord.errors.NotFound:
+            await channel.send(content=message_content.substitute(link_str_data), view=view)
 
 bot = Bot()
 
