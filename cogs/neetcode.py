40ac8b8f393ab6611bbdc3d8fe3bdc7819c5775a
Abe <abe@habet.dev>
2022-12-14 13:12:42 -0500

    change coding languages to autocompletion

--- cogs/neetcode.py
+++ cogs/neetcode.py
@@ -1,6 +1,5 @@
 import pathlib
-import re
-from typing import Literal, Optional
+from typing import List
 
 import discord
 import git
@@ -8,10 +7,6 @@ from discord import app_commands
 from discord.ext import commands
 from git.repo.base import Repo
 
-
-#languages = [x.name for x in self.neetcode.iterdir() if x.is_dir() and not x.name.startswith(".")]
-languages = ['c', 'cpp', 'csharp', 'go', 'java', 'javascript', 'kotlin', 'python', 'ruby', 'rust', 'scala', 'swift', 'typescript']
-print(languages)
 
 class Neetcode(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
@@ -21,7 +16,9 @@ class Neetcode(commands.Cog):
         self.neetcode = pathlib.Path("leetcode")
         self.neetcode.mkdir(exist_ok=True)
         try:
-            Repo.clone_from("https://github.com/self.neetcode-gh/leetcode.git", self.neetcode)
+            Repo.clone_from(
+                "https://github.com/self.neetcode-gh/leetcode.git", self.neetcode
+            )
             print("cloned repo")
         except git.exc.GitCommandError:
             repo = Repo(self.neetcode)
@@ -29,23 +26,44 @@ class Neetcode(commands.Cog):
             o.pull()
             print("pulled repo")
 
+        self.languages = [
+            x.name
+            for x in self.neetcode.iterdir()
+            if x.is_dir() and not x.name.startswith(".")
+        ]
 
     @app_commands.command()
     @app_commands.describe(
         number="the number leetcode problem you want a soluiton for",
-        language=", ".join(languages),
+        language="the coding language",
     )
-    async def leetcode(self, interaction: discord.Interaction, number: int, language: Literal['c', 'cpp', 'csharp', 'go', 'java', 'javascript', 'kotlin', 'python', 'ruby', 'rust', 'scala', 'swift', 'typescript']):
+    async def leetcode(
+        self, interaction: discord.Interaction, number: int, language: str
+    ):
         """Returns the leetcode solution"""
-        files = list(self.neetcode.glob(language+"/"+str(number)+"-*"))
-        if language not in languages or len(files) == 0:
-            await interaction.response.send_message(f'there are no solutions for leetcode problem #{number} in {language}')
+        files = list(self.neetcode.glob(language + "/" + str(number) + "-*"))
+        if language not in self.languages or len(files) == 0:
+            await interaction.response.send_message(
+                f"there are no solutions for leetcode problem #{number} in {language}"
+            )
             return
-        
+
         with open(files[0]) as f:
             code = f.read()
 
         await interaction.response.send_message(f"```{language}\n{code}\n```")
+
+    @leetcode.autocomplete("language")
+    async def leetcode_autocomplete(
+        self,
+        interaction: discord.Interaction,
+        current: str,
+    ) -> List[app_commands.Choice[str]]:
+        return [
+            app_commands.Choice(name=language, value=language)
+            for language in self.languages
+            if current.lower() in language.lower()
+        ]
 
 
 async def setup(bot: commands.Bot):
