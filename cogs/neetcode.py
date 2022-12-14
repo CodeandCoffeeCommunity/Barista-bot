2734c9787c960b46d96f1cb8e4197c94f52999cc
Abe <abe@habet.dev>
2022-12-13 21:44:54 -0500

    mv git to cog_load

--- cogs/neetcode.py
+++ cogs/neetcode.py
@@ -9,21 +9,25 @@ from discord.ext import commands
 from git.repo.base import Repo
 
 
-neetcode = pathlib.Path("leetcode")
-neetcode.mkdir(exist_ok=True)
-try:
-    Repo.clone_from("https://github.com/neetcode-gh/leetcode.git", neetcode)
-    print("cloned repo")
-except git.exc.GitCommandError:
-    repo = Repo(neetcode)
-    o = repo.remotes.origin
-    o.pull()
-    print("pulled repo")
-languages = [x.name for x in neetcode.iterdir() if x.is_dir() and not x.name.startswith(".")]
+#languages = [x.name for x in self.neetcode.iterdir() if x.is_dir() and not x.name.startswith(".")]
+languages = ['c', 'cpp', 'csharp', 'go', 'java', 'javascript', 'kotlin', 'python', 'ruby', 'rust', 'scala', 'swift', 'typescript']
+print(languages)
 
 class Neetcode(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+
+    async def cog_load(self) -> None:
+        self.neetcode = pathlib.Path("leetcode")
+        self.neetcode.mkdir(exist_ok=True)
+        try:
+            Repo.clone_from("https://github.com/self.neetcode-gh/leetcode.git", self.neetcode)
+            print("cloned repo")
+        except git.exc.GitCommandError:
+            repo = Repo(self.neetcode)
+            o = repo.remotes.origin
+            o.pull()
+            print("pulled repo")
 
 
     @app_commands.command()
@@ -33,7 +37,7 @@ class Neetcode(commands.Cog):
     )
     async def leetcode(self, interaction: discord.Interaction, number: int, language: Literal[*languages]):
         """Returns the leetcode solution"""
-        files = list(neetcode.glob(language+"/"+str(number)+"-*"))
+        files = list(self.neetcode.glob(language+"/"+str(number)+"-*"))
         if language not in languages or len(files) == 0:
             await interaction.response.send_message(f'there are no solutions for leetcode problem #{number} in {language}')
             return
