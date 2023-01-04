f8e35500a8449f1bc77998b61d0fffb3a27ff5f5
Abe <abe@habet.dev>
2023-01-04 16:10:11 -0500

    add all hands reminder

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -1,8 +1,8 @@
-import discord
 import datetime
-from discord.ext import commands
-from discord.ext import tasks
 from zoneinfo import ZoneInfo
+
+import discord
+from discord.ext import commands, tasks
 
 
 class LinkButton(discord.ui.View):
@@ -18,6 +18,7 @@ class Tasks(commands.Cog, name="tasks"):
 
         # start the task to run in the background
         self.monday_office_hour_reminder.start()
+        self.all_hands_reminder.start()
 
     @tasks.loop(time=datetime.time(hour=19, minute=30, tzinfo=ZoneInfo("America/New_York")))
     async def monday_office_hour_reminder(self):
@@ -29,7 +30,7 @@ class Tasks(commands.Cog, name="tasks"):
         office_hour_msg = """
 @here **Happening now**
 
-Staff Office hours - Weekly  
+Staff Office hours - Weekly
 
 Casual office hours. Come chat in the Google Meet with the different chapters. Ask questions, get help, meet each other, etc. fully up to you!
 """
@@ -38,6 +39,31 @@ Casual office hours. Come chat in the Google Meet with the different chapters. A
             view=LinkButton("Google meet", "https://meet.google.com/xnq-fdtk-ibh"),
         )
         print("sent")
+
+    @tasks.loop(time=datetime.time(hour=20, minute=25, tzinfo=ZoneInfo("America/New_York")))
+    async def all_hands_reminder(self):
+        now = datetime.datetime.now(tz=ZoneInfo("America/New_York"))
+        if now.day <= 7 and now.isoweekday() == 3:
+            channel = self.bot.get_channel(980353246361169950)
+            all_hands_msg = """
+@here **Happening now**
+
+Our **Monthly All Hands** meeting is **beginning in 5 minutes**.
+
+Starting at **8:30pm EST / 7:30pm CST / 5:30pm PST**
+
+After the meeting, we will be hosting an optional hangout session in the Discord voice channel from 9pm EST / 8pm CST / 6pm PST. This is a great opportunity to ask any questions you may have and connect with your fellow organizers and volunteers.
+
+We look forward to seeing you there!
+
+"""
+            await channel.send(
+                content=all_hands_msg,
+                view=LinkButton("Google meet", "https://meet.google.com/fgd-kknr-ogh"),
+            )
+            print("done")
+        else:
+            return
 
 
 async def setup(bot):
