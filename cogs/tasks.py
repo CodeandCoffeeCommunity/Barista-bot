83c75904b824245c54bc032386b2b43a9a9c611b
Abe <abe@habet.dev>
2023-01-05 21:30:49 -0500

    add local time to remider msg

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -20,43 +20,60 @@ class Tasks(commands.Cog, name="tasks"):
         self.monday_office_hour_reminder.start()
         self.all_hands_reminder.start()
 
-    @tasks.loop(time=datetime.time(hour=19, minute=30, tzinfo=ZoneInfo("America/New_York")))
+    @tasks.loop(time=datetime.time(hour=19, minute=30,
+                                   tzinfo=ZoneInfo("America/New_York")))
     async def monday_office_hour_reminder(self):
         now = datetime.datetime.now(tz=ZoneInfo("America/New_York"))
         if now.isoweekday() != 1:
             return
 
         channel = self.bot.get_channel(980353246361169950)
+        now = datetime.datetime.now(tz=ZoneInfo("America/New_York"))
+        time = datetime.time(hour=19, minute=30)
         office_hour_msg = """
 @here **Happening now**
 
-Staff Office hours - Weekly
+Staff Office hours - Weekly at {time}
 
 Casual office hours. Come chat in the Google Meet with the different chapters. Ask questions, get help, meet each other, etc. fully up to you!
-"""
+""".format(
+            time=discord.utils.format_dt(
+                datetime.datetime.combine(
+                    now,
+                    time),
+                style="t"))
         await channel.send(
             content=office_hour_msg,
             view=LinkButton("Google meet", "https://meet.google.com/xnq-fdtk-ibh"),
         )
         print("sent")
 
-    @tasks.loop(time=datetime.time(hour=20, minute=25, tzinfo=ZoneInfo("America/New_York")))
+    @tasks.loop(time=datetime.time(hour=20, minute=25,
+                tzinfo=ZoneInfo("America/New_York")))
     async def all_hands_reminder(self):
         now = datetime.datetime.now(tz=ZoneInfo("America/New_York"))
         if now.day <= 7 and now.isoweekday() == 3:
             channel = self.bot.get_channel(980353246361169950)
+            now = datetime.datetime.now(tz=ZoneInfo("America/New_York"))
+            time = datetime.time(hour=20, minute=30)
+
             all_hands_msg = """
 @here **Happening now**
 
 Our **Monthly All Hands** meeting is **beginning in 5 minutes**.
 
-Starting at **8:30pm EST / 7:30pm CST / 5:30pm PST**
+Starting at **{time}**
 
 After the meeting, we will be hosting an optional hangout session in the Discord voice channel from 9pm EST / 8pm CST / 6pm PST. This is a great opportunity to ask any questions you may have and connect with your fellow organizers and volunteers.
 
 We look forward to seeing you there!
 
-"""
+""".format(
+                time=discord.utils.format_dt(
+                    datetime.datetime.combine(
+                        now,
+                        time),
+                    style="t"))
             await channel.send(
                 content=all_hands_msg,
                 view=LinkButton("Google meet", "https://meet.google.com/fgd-kknr-ogh"),
