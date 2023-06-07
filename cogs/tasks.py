af3d28c2e3bdf6263cf871826a4bfbe7a37f2991
Abe <abe@habet.dev>
2023-06-07 15:42:06 -0400

    rm all hands reminder

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -18,7 +18,7 @@ class Tasks(commands.Cog, name="tasks"):
 
         # start the task to run in the background
         self.monday_office_hour_reminder.start()
-        self.all_hands_reminder.start()
+#        self.all_hands_reminder.start()
 
     @tasks.loop(time=datetime.time(hour=19, minute=30,
                                    tzinfo=ZoneInfo("America/New_York")))
@@ -50,41 +50,41 @@ Casual office hours. Come chat in the Google Meet with the different chapters. A
         )
         print("sent")
 
-    @tasks.loop(time=datetime.time(hour=20, minute=25,
-                                   tzinfo=ZoneInfo("America/New_York")))
-    async def all_hands_reminder(self):
-        now = datetime.datetime.now(tz=ZoneInfo("America/New_York"))
-        if now.day <= 7 and now.isoweekday() == 3:
-            channel = self.bot.get_channel(980353246361169950)
-            now = datetime.datetime.now(tz=ZoneInfo("America/New_York"))
-            time = datetime.time(hour=20, minute=30)
-
-            all_hands_msg = """
-@here **Happening now**
-
-Our **Monthly All Hands** meeting is **beginning in 5 minutes**.
-
-Starting at **{time}**
-
-After the meeting, we will be hosting an optional hangout session in the Discord voice channel from 9pm EST / 8pm CST / 6pm PST. This is a great opportunity to ask any questions you may have and connect with your fellow organizers and volunteers.
-
-We look forward to seeing you there!
-
-""".format(
-                time=discord.utils.format_dt(
-                    datetime.datetime.combine(
-                        now, time, tzinfo=ZoneInfo("America/New_York")
-                    ),
-                    style="t",
-                )
-            )
-            await channel.send(
-                content=all_hands_msg,
-                view=LinkButton("Google meet", "https://meet.google.com/fgd-kknr-ogh"),
-            )
-            print("done")
-        else:
-            return
+#    @tasks.loop(time=datetime.time(hour=20, minute=25,
+#                                   tzinfo=ZoneInfo("America/New_York")))
+#    async def all_hands_reminder(self):
+#        now = datetime.datetime.now(tz=ZoneInfo("America/New_York"))
+#        if now.day <= 7 and now.isoweekday() == 3:
+#            channel = self.bot.get_channel(980353246361169950)
+#            now = datetime.datetime.now(tz=ZoneInfo("America/New_York"))
+#            time = datetime.time(hour=20, minute=30)
+#
+#            all_hands_msg = """
+#@here **Happening now**
+#
+#Our **Monthly All Hands** meeting is **beginning in 5 minutes**.
+#
+#Starting at **{time}**
+#
+#After the meeting, we will be hosting an optional hangout session in the Discord voice channel from 9pm EST / 8pm CST / 6pm PST. This is a great opportunity to ask any questions you may have and connect with your fellow organizers and volunteers.
+#
+#We look forward to seeing you there!
+#
+#""".format(
+#                time=discord.utils.format_dt(
+#                    datetime.datetime.combine(
+#                        now, time, tzinfo=ZoneInfo("America/New_York")
+#                    ),
+#                    style="t",
+#                )
+#            )
+#            await channel.send(
+#                content=all_hands_msg,
+#                view=LinkButton("Google meet", "https://meet.google.com/fgd-kknr-ogh"),
+#            )
+#            print("done")
+#        else:
+#            return
 
 
 async def setup(bot):
